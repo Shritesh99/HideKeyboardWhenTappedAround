ce8502be8fb4c30cb51cd1c6cb4d9ced778c7db8
Shritesh <shritesh.sj@gmail.com>
2019-12-22 01:00:50 +0530

    Initial Commit

---
+++ HideKeyboardWhenTappedAround/HideKeyboardWhenTappedAround.h
@@ -0,0 +1,19 @@
+//
+//  HideKeyboardWhenTappedAround.h
+//  HideKeyboardWhenTappedAround
+//
+//  Created by Shritesh Jamulkar on 22/12/19.
+//  Copyright © 2019 Shritesh Jamulkar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HideKeyboardWhenTappedAround.
+FOUNDATION_EXPORT double HideKeyboardWhenTappedAroundVersionNumber;
+
+//! Project version string for HideKeyboardWhenTappedAround.
+FOUNDATION_EXPORT const unsigned char HideKeyboardWhenTappedAroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HideKeyboardWhenTappedAround/PublicHeader.h>
+
+
